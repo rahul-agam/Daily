47efd4403b2a0989df275d5e8da02fc9c5f9e448
rahul-agam <hidekiryuga0402@gmail.com>
2024-04-05 23:07:35 +0530

    create a test API

--- Daily-API/index.js
+++ Daily-API/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ mongoose
   });
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!!!");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000!!!");
 });
+
+app.use("/api/user", userRoutes);
